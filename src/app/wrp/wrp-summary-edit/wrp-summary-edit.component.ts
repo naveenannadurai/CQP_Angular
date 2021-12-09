4d05fc0c31b8e7ceb8a7c8d6024c61483984ee30
naveenannadurai <naveen.annadurai@lntinfotech.com>
2021-12-09 13:38:56 +0530

    Merge Changes Updated

--- src/app/wrp/wrp-summary-edit/wrp-summary-edit.component.ts
+++ src/app/wrp/wrp-summary-edit/wrp-summary-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
 
 @Component({
   selector: 'app-wrp-summary-edit',
@@ -9,11 +10,40 @@ import { NgModule } from '@angular/core';
 })
 export class WrpSummaryEditComponent implements OnInit {
 
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings = {};
+
   constructor(private router: Router) { }
 
   ngOnInit() {
+    this.dropdownList = [
+      { item_id: 1, item_text: 'MTC1' },
+      { item_id: 2, item_text: 'MTC2' },
+      { item_id: 3, item_text: 'MTC3' },
+      { item_id: 4, item_text: 'MTC4' },
+      { item_id: 5, item_text: 'MTC5' }
+    ];
+    this.selectedItems = [
+      
+    ];
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
   }
   
+  onItemSelect(item: any) {
+    console.log(item);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
   
   assignMTC(){
     this.router.navigate(['/assign-mtc']);
