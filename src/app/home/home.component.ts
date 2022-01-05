3eeb89cd0ec2b08517f3c44189bc4294f893dd62
naveenannadurai <naveen.annadurai@lntinfotech.com>
2022-01-05 14:07:51 +0530

    Hide/Show, Copyright is implemented in homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChildren, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChildren, OnInit, ElementRef } from '@angular/core';
 import { Alert } from '../model/alert';
 import { AlertService } from '../service/alert.service';
 import { HomeDialogComponent } from './home.dialog.component';
@@ -6,7 +6,6 @@ import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { DownloadService } from "../service/download.service";
 import { saveAs } from 'file-saver';
-import { throwError } from 'rxjs';
 
 //import { jsonpFactory } from '@angular/http/src/http_module';
 @Component({
@@ -17,8 +16,9 @@ import { throwError } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
 
-  private errorResponse: any;
-  constructor(private router: Router, private alertService: AlertService, public dialog: MatDialog, private DownloadService: DownloadService) { }
+  constructor(private router: Router,
+    private alertService: AlertService,
+    public dialog: MatDialog) { }
   private inbox: any;
   public vouchers = new Array();
   public users: any;
@@ -31,6 +31,51 @@ export class HomeComponent implements OnInit {
     this.getinbox();
     this.getAlerts();
   }
+  vouchertbl: boolean = true;
+  callintbl: boolean = true;
+  tpltbl: boolean = true;
+  usertbl: boolean = true;
+  vouchercosttbl: boolean = true;
+  show($event) {
+    switch ($event.target.getAttribute('name')) {
+      case 'vouchertbl': {
+        this.vouchertbl = !this.vouchertbl
+        this.showHide(this.vouchertbl, $event)
+        break;
+      }
+      case 'callintbl': {
+        this.callintbl = !this.callintbl
+        this.showHide(this.callintbl, $event)
+        break;
+      }
+      case 'tpltbl': {
+        this.tpltbl = !this.tpltbl
+        this.showHide(this.tpltbl, $event)
+        break;
+      }
+      case 'usertbl': {
+        this.usertbl = !this.usertbl
+        this.showHide(this.usertbl, $event)
+        break;
+      }
+      case 'vouchercosttbl': {
+        this.vouchercosttbl = !this.vouchercosttbl
+        this.showHide(this.vouchercosttbl, $event)
+        break;
+      }
+    }
+  }
+  showHide(tblFlag, $event) {
+    console.log($event.target.setAttribute)
+    if (tblFlag) {
+      $event.target.setAttribute("src", "../../assets/hide.gif");
+      $event.target.setAttribute("title", "hide");
+    } else {
+      $event.target.setAttribute("src", "../../assets/show.gif");
+      $event.target.setAttribute("title", "show");
+    }
+  }
+
   export(fileName: string) {
     this.alertService.export(fileName).subscribe(
       blob => saveAs(blob, fileName + "-Sheet.xlsx")
